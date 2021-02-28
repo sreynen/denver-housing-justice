3df054403e35b4c74556af40b475ea891701232b
Scott Reynen <scott.reynen@moveon.org>
2021-02-28 15:14:06 -0700

    Fix keyword matching

--- scrape_news.py
+++ scrape_news.py
@@ -17,20 +17,21 @@ json_feed = {
 }
 
 filter_key_phrases = [
-    ['house', 'prices'], ['housing'],
-    ['homeless'], ['homelessness'],
-    ['evict'], ['eviction'],
-    ['rent'],
-    ['property', 'tax'], ['property', 'taxes'],
+    'house prices', 'housing',
+    'homeless', 'homelessness',
+    'evict', 'eviction',
+    'rent',
+    'property tax', 'property taxes',
 ]
 response = requests.get('https://sreynen.github.io/denver-news/feed.json')
 source_json_feed = json.loads(response.text)
 for item in source_json_feed.get('items'):
     match = False
     title_words = re.findall(r"[\w']+", item.get('title', ''))
-    title_string = ' '.join([word.lower() for word in title_words])
+    title_substring = ' '.join([word.lower() for word in title_words]).lower()
+    title_string = f" {title_substring} "
     for phrase in filter_key_phrases:
-        phrase_string = ' '.join(phrase)
+        phrase_string = f" {phrase} "
         if phrase_string in title_string:
             match = True
     if match:
